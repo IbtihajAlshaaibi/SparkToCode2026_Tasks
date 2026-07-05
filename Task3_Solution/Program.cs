5b07d49f7e461d555c7e6b2eeb93bc4bfa61b850
Ibtihaj <36j207@utas.edu.om>
2026-07-05 23:19:58 +0400

    Task 2 - Power & Root Explorer

--- Task3_Solution/Program.cs
+++ Task3_Solution/Program.cs
@@ -6,17 +6,31 @@
         {
             // Task 1 - Absolute Difference
 
-            Console.WriteLine("Enter the first number:");
-            float firstNumber = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the first number:");
+            //float firstNumber = float.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter the second number:");
-            float secondNumber = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the second number:");
+            //float secondNumber = float.Parse(Console.ReadLine());
 
-            float difference = firstNumber - secondNumber;
+            //float difference = firstNumber - secondNumber;
 
-            float positiveDifference = Math.Abs(difference);
+            //float positiveDifference = Math.Abs(difference);
 
-            Console.WriteLine("Positive Difference: " + positiveDifference);
+            //Console.WriteLine("Positive Difference: " + positiveDifference);
+
+            //////////////////////////////////////////////////////////////////////////////////////
+
+            // Task 2 - Power & Root Explorer
+
+            Console.WriteLine("Enter a number:");
+            float number = float.Parse(Console.ReadLine());
+
+            double square = Math.Pow(number, 2);
+
+            double squareRoot = Math.Sqrt(number);
+
+            Console.WriteLine("Square: " + square);
+            Console.WriteLine("Square Root: " + squareRoot);
         }
     }
 }
