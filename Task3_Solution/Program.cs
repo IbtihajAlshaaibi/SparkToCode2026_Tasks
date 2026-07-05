f07b212693335915f5e5e5534e08547b0f853545
Ibtihaj <36j207@utas.edu.om>
2026-07-05 23:21:47 +0400

    Task 3 - Name Formatter

--- Task3_Solution/Program.cs
+++ Task3_Solution/Program.cs
@@ -22,15 +22,33 @@
 
             // Task 2 - Power & Root Explorer
 
-            Console.WriteLine("Enter a number:");
-            float number = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter a number:");
+            //float number = float.Parse(Console.ReadLine());
 
-            double square = Math.Pow(number, 2);
+            //double square = Math.Pow(number, 2);
 
-            double squareRoot = Math.Sqrt(number);
+            //double squareRoot = Math.Sqrt(number);
 
-            Console.WriteLine("Square: " + square);
-            Console.WriteLine("Square Root: " + squareRoot);
+            //Console.WriteLine("Square: " + square);
+            //Console.WriteLine("Square Root: " + squareRoot);
+
+            //////////////////////////////////////////////////////////////////////////////////////
+
+            //Task 3 - Name Formatter
+
+            Console.WriteLine("Enter your full name:");
+            string fullName = Console.ReadLine();
+
+            string upperName = fullName.ToUpper();
+
+            string lowerName = fullName.ToLower();
+
+            int characters = fullName.Length;
+
+            Console.WriteLine("Uppercase: " + upperName);
+            Console.WriteLine("Lowercase: " + lowerName);
+            Console.WriteLine("Number of Characters: " + characters);
+
         }
     }
 }
