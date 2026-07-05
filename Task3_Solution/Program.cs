9df49f0f87db8b125494f68ad0dc3f5f976bc4f8
Ibtihaj <36j207@utas.edu.om>
2026-07-05 23:17:48 +0400

    Task 1 - Absolute Difference

--- Task3_Solution/Program.cs
+++ Task3_Solution/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Task 1 - Absolute Difference
+
+            Console.WriteLine("Enter the first number:");
+            float firstNumber = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+            float secondNumber = float.Parse(Console.ReadLine());
+
+            float difference = firstNumber - secondNumber;
+
+            float positiveDifference = Math.Abs(difference);
+
+            Console.WriteLine("Positive Difference: " + positiveDifference);
         }
     }
 }
