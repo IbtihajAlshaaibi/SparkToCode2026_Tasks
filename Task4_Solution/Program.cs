db1555e3930f9aa924bf480b50bddf87ea973a3d
Ibtihaj <36j207@utas.edu.om>
2026-07-06 22:33:53 +0400

    Task 2 - Square Number Function

--- Task4_Solution/Program.cs
+++ Task4_Solution/Program.cs
@@ -2,19 +2,33 @@
 {
     internal class Program
     {
-        //Task 1 - Personalized Welcome Function
+        //Task 1 
         static void PrintWelcome(string name)
         {
             Console.WriteLine("Welcome " + name + "!");
             
         }
+
+        // Task 2
+        static int Square(int number)
+        {
+            return number * number;
+        }
+
         static void Main(string[] args)
         {
+            // Task 1
             Console.Write("Enter your name: ");
             string userName = Console.ReadLine();
-
-
             PrintWelcome(userName);
+
+
+            // Task 2
+            Console.Write("Enter a number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            int result = Square(num);
+            Console.WriteLine("The square is: " + result);
         }
     }
 }
