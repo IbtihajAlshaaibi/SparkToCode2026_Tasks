c6aceb654f9793100968122fa58d6fbbeeabcc95
Ibtihaj <36j207@utas.edu.om>
2026-07-06 22:21:46 +0400

    add project files

---
+++ Task4_Solution/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task4_Solution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
